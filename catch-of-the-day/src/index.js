c5d75f8d89804311a84a52f46cebb3d19d4a32e6
Petter Carlsson <pettercarlsson@Petters-iMac.local>
2020-10-12 16:33:02 +0200

    Created StorePicker component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,2 +1,9 @@
-// let's go!
-// test
+import React from 'react';
+import { render } from 'react-dom';
+// Import components
+import StorePicker from './components/StorePicker';
+// Import CSS
+import './css/style.css';
+
+
+render(<StorePicker />, document.querySelector('#main'));
