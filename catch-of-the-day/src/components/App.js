952bbf832521e27594307052baabb817e9ee5954
Petter Carlsson <pettercarlsson@Petters-iMac.local>
2020-10-13 16:46:02 +0200

    Add Fish component to App.js

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
+import Fish from './Fish';
+// Import sample fishes
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
     // Set initial state (when App loads)
@@ -20,14 +23,21 @@ class App extends React.Component {
         this.setState({fishes: fishes});
     }
 
+    loadSampleFishes = () => {
+        this.setState({ fishes: sampleFishes });
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+                    </ul>
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish}/>
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
             </div>
         )
     }
