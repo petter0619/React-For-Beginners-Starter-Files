90e8e5253b2178d5fca7706d5b2e3ea9b4d8dae0
Petter Carlsson <pettercarlsson@Petters-iMac.local>
2020-10-14 14:51:37 +0200

    Remove items from order

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -84,6 +84,15 @@ class App extends React.Component {
         this.setState({ order: order });
     }
 
+    removeFromOrder = (key) => {
+        // 1. Take a copy of state
+        const order = {...this.state.order};
+        // 2. Remove item from order
+        delete order[key];
+        // 3. Call setState to update our state object
+        this.setState({ order: order });
+    }
+
     // -------------------- RENDER --------------------
     render() {
         return (
@@ -101,7 +110,11 @@ class App extends React.Component {
                         ))}
                     </ul>
                 </div>
-                <Order fishes={this.state.fishes} order={this.state.order}/>
+                <Order 
+                    fishes={this.state.fishes} 
+                    order={this.state.order} 
+                    removeFromOrder={this.removeFromOrder}
+                />
                 <Inventory 
                     addFish={this.addFish} 
                     loadSampleFishes={this.loadSampleFishes}
